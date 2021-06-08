0d9d16d73e0879124ce9a81b054635588848dcbb
faissal <faissal.devtest@gmail.com>
2021-06-08 23:27:17 +0100

    home et welcome interfaces ajoutés et user authentification using google

--- server.js
+++ server.js
@@ -1,42 +1,83 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
+const passport = require('passport');
+const cookieSession = require('cookie-session')
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+require('./passport-setup');
+
+const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-  debug: true
+  debug: true,
 });
-const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer);
+// For an actual app you should configure this with an experation time, better keys, proxy and secure
+app.use(cookieSession({
+    name: 'tuto-session',
+    keys: ['key1', 'key2']
+  }))
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine','ejs')
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+// Initializes passport and passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Example protected and unprotected routes
+app.use('/public', express.static('public'));
+app.use("/peerjs", peerServer);
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+// In this route you can see that if the user is logged in u can acess his info in: req.user
+app.get('/home', isLoggedIn, (req, res) =>{
+    res.render("pages/home",{name:req.user.displayName,pic:req.user.photos[0].value,email:req.user.emails[0].value})
 })
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/home');
+  }
+);
+
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
 })
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-  }); 
+app.get("/room", (req, rsp) => {
+  rsp.redirect(`/room${uuidv4()}`);
+});
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+app.get("/room:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-server.listen(process.env.PORT||3030)
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
